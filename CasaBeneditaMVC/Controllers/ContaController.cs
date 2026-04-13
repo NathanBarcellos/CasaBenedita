5941ee9bd7873b52d4c4f05e69d215b14abd6d69
NATHAN BARCELLOS DE ARAUJO FERREIRA <nathan.baferreira@senacsp.edu.br>
2026-04-13 16:45:25 -0300

    Primeiro commit CasaBenedita

---
+++ CasaBeneditaMVC/Controllers/ContaController.cs
@@ -0,0 +1,29 @@
+﻿using CasaBeneditaMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaBeneditaMVC.Controllers
+{
+    public class ContaController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ContaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Usuarios.Add(usuario);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return BadRequest();
+        }
+    }
+}
---
+++ CasaBeneditaMVC/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CasaBeneditaMVC.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Senha { get; set; }
+    }
+}
