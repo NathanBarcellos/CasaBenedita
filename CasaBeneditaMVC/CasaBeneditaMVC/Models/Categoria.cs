f9aff23d656192eb4b81e8be9dce21ae1cbcc5d1
NATHAN BARCELLOS DE ARAUJO FERREIRA <nathan.baferreira@senacsp.edu.br>
2026-04-17 16:08:00 -0300

    Atualização do MVC

---
+++ CasaBeneditaMVC/CasaBeneditaMVC/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CasaBeneditaMVC.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nome { get; set; }
+
+        public List<Produto> Produtos { get; set; }
+    }
+}
